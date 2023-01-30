a4c6ec3f926672e8cd800e528701efd6ff052084
Giulia de Carvalho <giuliacarvalhof@gmail.com>
2023-01-30 19:14:16 -0300

    up project

---
+++ android/app/src/main/kotlin/com/example/pix_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pix_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,50 @@
+class Strings {
+
+  //==================home screen===================
+    //header
+    static String accountBalance = 'Saldo na conta';
+    static String balanceAvailable = 'Saldo disponível';
+    static String boxBalance = 'Saldo nas caixinhas';
+    static String symbolMoney = 'R\$';
+
+    //pay
+    static String copyPaste = 'Pix Copia e Cola';
+    static String optionsPix = 'CPF/CNPJ, e-mail, telefone ou outra chave';
+    static String pay = 'Pagar';
+    static String paySlip = 'Pagar Boleto';
+    static String readQRCode = 'Ler QR CODE';
+    static String usePixKey = 'Usar chave Pix';
+
+    //receive
+    static String generateQRCode = 'Gerar QR Code Permanente';
+    static String generateQRCodeSubtitle = 'QR Code que sempre está disponível';
+    static String generateSingleQRCode = 'Gerar QR CODE de uso único';
+    static String receive = 'Receber';
+    static String receivePix = 'Receber com Chave Pix';
+
+    //others
+    static String configLimitPix = 'Configurar Limites Pix';
+    static String registerOrBringKey = 'Registrar ou trazer chaves';
+    static String scheduledTransfer = 'Transferências Agendadas';
+    static String keyPix = 'Usar chave pix';
+  //======================================
+
+  //===============limit transaction screen====================
+    // header
+    static String transactionLimit = 'Limites das transações Pix';
+    static String transactionLimitSubtitle = 'Você pode ajustar os limites de transacão Pix durante o dia e no período noturno.';
+
+    //day transactions
+    static String limitDay = 'Limite do período diurno';
+    static String limitDayMax = 'Limite máximo de transação diurna';
+
+    //night transactions
+    static String limitNight = 'Limite do período noturno';
+    static String limitNightMax = 'Limite máximo de transação noturna';
+    static String nightTime = 'Horário Noturno';
+
+    //others
+    static String saveButton = 'SALVAR';
+  //======================================
+
+}
